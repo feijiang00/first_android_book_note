7094df873c8746d6fec349b7aca8bc2869f877f7
fejjiang00 <postmaster@jiangkk.top>
2022-08-26 10:07:41 +0800

    first add coed

---
+++ main/java/com/example/helloworld/view/ClockActivity.kt
@@ -0,0 +1,15 @@
+package com.example.helloworld.view
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import com.example.helloworld.R
+
+class ClockActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_clock)
+
+    }
+}
